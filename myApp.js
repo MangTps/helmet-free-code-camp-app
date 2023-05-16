0c71a5060d3838dc1c92dd6d27ae1217cd29c055
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2023-05-16 16:24:22 +0700

    feat:  app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));;

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
+var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.hidePoweredBy());
 module.exports = app;
 const api = require("./server.js");
