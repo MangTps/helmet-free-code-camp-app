86580be64f3749c88d4579992e99d7826b493ce6
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2023-05-16 16:37:27 +0700

    feat:  app.use(helmet.contentSecurityPolicy());

--- myApp.js
+++ myApp.js
@@ -1,25 +1,52 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
-var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
+// Set security-related HTTP headers
 app.use(helmet());
+
+// Enable HSTS (HTTP Strict Transport Security)
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+
+// Hide "X-Powered-By" header
 app.use(helmet.hidePoweredBy());
+
+// Prevent DNS prefetching
 app.use(helmet.dnsPrefetchControl());
+
+// Disable client-side caching
 app.use(helmet.noCache());
-module.exports = app;
-const api = require("./server.js");
+
+// Serve static files from the "public" directory
 app.use(express.static("public"));
+
+// Disable Strict-Transport-Security header
 app.disable("strict-transport-security");
+
+// Set X-Frame-Options to deny
 app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.xssFilter({}));
+
+// Enable XSS (Cross-Site Scripting) protection
+app.use(helmet.xssFilter());
+
+// Prevent MIME type sniffing
 app.use(helmet.noSniff());
+
+// Set X-Download-Options to prevent Internet Explorer from executing downloads
 app.use(helmet.ieNoOpen());
+
+// Mount your API routes
+const api = require("./server.js");
 app.use("/_api", api);
+
+// Define a route for the homepage
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
+
+// Start the server
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
-  console.log(`🍢 Infomation Security App started on port ${port}`);
+  console.log(`🍢 Information Security App started on port ${port}`);
 });
