b7b47fb7a51f135aec23ea02dbc732017570550f
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2023-05-16 16:02:17 +0700

    feat: Hide powered by fix

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
 app.disable("strict-transport-security");
+app.use(helmet.frameguard({ action: "deny" }));
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
