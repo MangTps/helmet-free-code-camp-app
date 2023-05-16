d839da4074bca82ac75cd23fd9dbaf096471382d
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2023-05-16 16:05:43 +0700

    feat: app.use(helmet.xssFilter({}));

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ const api = require("./server.js");
 app.use(express.static("public"));
 app.disable("strict-transport-security");
 app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter({}));
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
